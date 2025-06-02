0cb2e087c0959279551e41018b9543a25d92e867
AMAURI BARBIERI FILHO <0201392411034>
2025-06-02 11:37:19 -0300

    Funcionalidade de Reserva implementadas, checa se há pelo menos um exemplar disponível. Metodo InserirLivroTeste colocado em comentário no LivroController para testes.

--- Fatec-Library/Controllers/LivroController.cs
+++ Fatec-Library/Controllers/LivroController.cs
@@ -22,30 +22,38 @@ namespace Fatec_Library.Controllers
             return View(livro);
         }
 
-        public async Task<IActionResult> InserirLivroTeste()
-        {
-            var livro = new Livro
-            {
-                Titulo = "Estruturas de Dados em C#",
-                Autores = new List<string> { "Fulano de Tal", "Beltrano da Silva" },
-                Editora = "Editora Fatec",
-                Descricao = "Livro de teste sobre estruturas de dados.",
-                Ano_Publicacao = 2023,
-                Isbn = "978-85-333-1234-5",
-                Idioma = "Português",
-                Num_Paginas = 350,
-                Capa_Livro = "https://examplofoda.com/capa.jpg",
-                Area = null,
-                Codigo_Exemplar = new List<int> { 1001, 1002, 1003 }
-            };
+        /*   public async Task<IActionResult> InserirLivroTeste()
+           {
+               var livro = new Livro
+               {
+                   Titulo = "Estruturas de Dados em JavaScript",
+                   Autores = new List<string> { "Fulano de Tal", "Beltrano da Silva" },
+                   Editora = "Editora Fatec",
+                   Descricao = "Livro de teste sobre estruturas de dados.",
+                   Ano_Publicacao = 2023,
+                   Isbn = "978-85-333-1234-5",
+                   Idioma = "Português",
+                   Num_Paginas = 350,
+                   Capa_Livro = "https://examplofoda.com/capa.jpg",
+                   Area = null,
+                   Codigo_Exemplar = new List<int> { 1001, 1002, 1003 }
+               };
 
+               await _context.Livros.InsertOneAsync(livro);
 
+               var exemplares = new List<Exemplar>
+       {
+           new Exemplar { Codigo_Exemplar = 1001, Status_Exemplar = "Disponível", Livro = livro },
+           new Exemplar { Codigo_Exemplar = 1002, Status_Exemplar = "Disponível", Livro = livro },
+           new Exemplar { Codigo_Exemplar = 1003, Status_Exemplar = "Disponível", Livro = livro }
+       };
 
+               await _context.Exemplares.InsertManyAsync(exemplares);
 
-            await _context.Livros.InsertOneAsync(livro);
+               TempData["Sucesso"] = "Livro de teste e exemplares inseridos com sucesso 2.0!";
+               return RedirectToAction("Listar");
+           }
 
-            TempData["Sucesso"] = "Livro de teste cadastrado com sucesso!";
-            return RedirectToAction("Listar");
-        }
+       } */
     }
 }
